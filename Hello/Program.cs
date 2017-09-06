20088aaad8a537ad387304471c58799298896796
K2832 <K2832@student.jamk.fi>
2017-09-06 10:41:01 +0300

    added blackjack.cs

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -17,7 +17,7 @@ namespace Hello
       //SayHello();
       //ShowNames();
       //staattisen ohjelman kutsuminen toisesta luokasta
-      Hello.Lotto.DrawLotto();
+      Hello.blackjack.Blackjack();
     }
 
     static void ShowNames()
