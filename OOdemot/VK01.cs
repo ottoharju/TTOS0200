e3573719572ef75d65966e5a5d611bcb238b4ebb
Otto <K2832@student.jamk.fi>
2017-09-12 20:50:59 +0300

    Fine product

--- OOdemot/VK01.cs
+++ OOdemot/VK01.cs
@@ -8,5 +8,21 @@ namespace JAMK.IT
 {
     class VK01
     {
-    }
+        
+ public static void Main(String []arg)
+ {
+        float a,b,c,summa,keskiarvo;
+        Console.WriteLine("Syota kolme numeroa: ");
+        a=Convert.ToInt32(Console.ReadLine());
+        b=Convert.ToInt32(Console.ReadLine());
+        c=Convert.ToInt32(Console.ReadLine());
+        //summan lasku
+        summa=a+b+c;
+        //keskiarvon lasku
+        keskiarvo=summa/3;
+        Console.WriteLine("\nSumma= " + summa);
+        Console.WriteLine("\nKeskiarvo= " + keskiarvo);
+ }
 }
+    }
+
