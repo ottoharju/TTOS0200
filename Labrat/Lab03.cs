a5ab3a84f7ebb3bcf562d894e87b3da0afbcdb42
K2832 <K2832@student.jamk.fi>
2017-09-20 10:57:07 +0300

    Begin Lab03

---
+++ Labrat/Lab03.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labrat
+{
+    class Lab03
+    {
+        public class T1
+        {
+            //Properties
+            string Syote { get; set; }
+            string Pva { get; set; }
+            //Constructors
+
+        }
+    }
+}
