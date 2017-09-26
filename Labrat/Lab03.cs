1914583778dcee3b743eb11d4eb82e8945d2748c
Otto <K2832@student.jamk.fi>
2017-09-26 21:04:07 +0300

    Finished 2 & 3

--- Labrat/Lab03.cs
+++ Labrat/Lab03.cs
@@ -46,7 +46,244 @@ namespace Labrat
                 }
                 
             }
-        }
+        }        public class Tehtava2
         
+        public class Hissi
+
+        {
+
+            public int TamaKerros { get; set; }
+
+
+
+            public Hissi()
+
+            {
+
+                this.TamaKerros = TamaKerros;
+
+                this.UusiKerros = UusiKerros;
+
+            }
+
+
+
+            private int uusikerros=1;
+
+            public int UusiKerros
+
+            {
+
+                get
+
+                {
+
+                    return uusikerros;
+
+                }
+
+                set
+
+                {
+
+                    if (value > 0 && value < 6)
+
+                    {
+
+                        uusikerros = value;
+
+                    }
+
+                    else if (value < 1)
+
+                    {
+
+                        Console.WriteLine("Liian pieni kerros!");
+
+                    }
+
+                    else if (value > 5)
+
+                    {
+
+                        Console.WriteLine("Liian suuri kerros!");
+
+                    }
+
+                }
+
+            }
+
+
+
+            public int AsetaKerros()
+
+            {
+
+                Console.Write("Anna uusi kerros (1-5) > ");
+
+                UusiKerros = int.Parse(Console.ReadLine());
+
+                return UusiKerros;
+
+            }
+
+
+
+            public void TulostaKerros()
+
+            {
+
+                TamaKerros = UusiKerros;
+
+                Console.WriteLine("Hissi on nyt kerroksessa " + TamaKerros);
+
+            }
+
+
+
+            public static void TestiHissi()
+
+            {
+
+                Hissi dynamo = new Hissi();
+
+                while (true)
+
+                {
+
+                    dynamo.TulostaKerros();
+
+                    dynamo.AsetaKerros();
+
+                }
+
+            }
+
+        }
+
+
+
+        public class Vahvistin
+
+        {
+
+            public int VolumeNyt { get; set; }
+
+            public Vahvistin()
+
+            {
+
+                this.VolumeNyt = VolumeNyt;
+
+                this.VolumeUusi = VolumeUusi;
+
+            }
+
+
+
+            private int volumeuusi;
+
+            public int VolumeUusi
+
+            {
+
+                get
+
+                {
+
+                    return volumeuusi;
+
+                }
+
+                set
+
+                {
+
+                    if (value >= 0 && value <= 100)
+
+                    {
+
+                        volumeuusi = value;
+
+                    }
+
+                    else if (value < 0)
+
+                    {
+
+                        Console.WriteLine("Liian pieni äänenvoimakkuus! Asetetaan minimiin: 0");
+
+                        volumeuusi = 0;
+
+                    }
+
+                    else if (value > 100)
+
+                    {
+
+                        Console.WriteLine("Liian suuri äänenvoimakkuus! Asetetaan maksimiin: 100");
+
+                        volumeuusi = 100;
+
+                    }
+
+                }
+
+            }
+
+
+
+            public int AsetaVolume()
+
+            {
+
+                Console.Write("Anna uusi äänenvoimakkuus (0-100) > ");
+
+                VolumeUusi = int.Parse(Console.ReadLine());
+
+                return VolumeUusi;
+
+            }
+
+
+
+            public void TulostaVolume()
+
+            {
+
+                VolumeNyt = VolumeUusi;
+
+                Console.WriteLine("Äänenvoimakkuus on nyt asetettu: " + VolumeNyt);
+
+            }
+
+
+
+            public static void TestiVahvistin()
+
+            {
+
+                Vahvistin onkyo = new Vahvistin();
+
+                while (true)
+
+                {
+
+                    onkyo.AsetaVolume();
+
+                    onkyo.TulostaVolume();
+
+                }
+
+            }
+
+
+
+
+
+        }
+
     }
+
 }
