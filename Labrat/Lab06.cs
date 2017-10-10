ee515c19d44686d6886fe984129e3f36079ac66d
Otto <K2832@student.jamk.fi>
2017-10-10 16:17:19 +0300

    Lab06 added

---
+++ Labrat/Lab06.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labrat
+{
+    class Lab06
+    {
+        public class Tehtava1
+        {
+
+        }
+    }
+}
