56116b2f189c619c44c46919406dfc3472fbad1b
K2832 <K2832@student.jamk.fi>
2017-11-01 08:44:48 +0200

    VK44 added

---
+++ Labrat/VK44.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labrat
+{
+    class JAMK
+    {
+        static void LueOhjastajatVer1()
+        {
+            try
+            {
+                catch (Exception)
+            }
+        }
+    }
+}
