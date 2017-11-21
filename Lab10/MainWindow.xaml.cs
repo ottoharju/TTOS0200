ccd00296bc1b0524491a365ca67ceb588fb6e172
Otto <K2832@student.jamk.fi>
2017-11-21 20:22:26 +0200

    Lab11 Task 1 added

---
+++ Lab10/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuy_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string shoppinglist = null;
+                if ((bool)chb_1.IsChecked)
+                {
+                    shoppinglist += "Milk ";
+                }
+                if ((bool)chb_2.IsChecked)
+                {
+                    shoppinglist += "Butter ";
+                }
+                if ((bool)chb_3.IsChecked)
+                {
+                    shoppinglist += "Beer ";
+                }
+                if ((bool)chb_4.IsChecked)
+                {
+                    shoppinglist += "Chicken ";
+                }
+                if ((bool)chb_5.IsChecked)
+                {
+                    shoppinglist += "Lemonade ";
+                }
+                txbShoppinglist.Text = shoppinglist;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
